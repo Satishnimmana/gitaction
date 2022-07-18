ff49e143835a2e080758e956932a2658ea3a9569
Satishnimmana <satishnimmana31@gamil.com>
2022-07-18 20:05:07 +0530

    new code

---
+++ Main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.12"
+}
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-098e42ae54c764c35" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
