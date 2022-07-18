f23726837a7f040774d93719f6d3bbc73515d5e6
Satishnimmana <98081327+Satishnimmana@users.noreply.github.com>
2022-07-18 21:21:23 +0530

    Delete Main.tf

--- Main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 0.12"
-}
-# Resource Block
-resource "aws_instance" "ec2demo" {
-  ami           = "ami-098e42ae54c764c35" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
-}
