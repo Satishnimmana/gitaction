7c1aad5da22bce91a76fb3ba0d925ebcb328db1d
Satishnimmana <satishnimmana31@gamil.com>
2022-07-18 20:19:01 +0530

    code

--- Main.tf
+++ Main.tf
@@ -8,6 +8,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-098e42ae54c764c35" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-098e42ae54c764c35"  # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
